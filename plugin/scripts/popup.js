59af6917b758bf45e63db2bcfa12004e2209af3b
Jagdeep Singh <jagdeeps1@Jagdeep-Singh.local>
2018-06-30 03:16:45 +0530

    changes

--- plugin/scripts/popup.js
+++ plugin/scripts/popup.js
@@ -1,11 +1,6 @@
 (function () {
-    console.log("Loaded");
-
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-    });
-
+    var base_url = "http://172.19.5.53:8080/";
+    var loggedInUserId;
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
             console.log("received msg back");
@@ -16,10 +11,86 @@
         }
     );
 
+    document.getElementById("loginBtn").addEventListener("click", function(){
+      console.log("Login Clicked");
+      doLogin();
+    });
+
+    document.getElementById("open_techmark").addEventListener("click", function(){
+      window.open("http://localhost:4200/#/user/home?userId="+loggedInUserId);
+    });
+
+    init();
+
+    function init(){
+      chrome.storage.local.get(['userObj'], function(result) {
+        console.log('Value currently is ' + JSON.stringify(result));
+        if(result.userObj){
+          loggedInUserId = result.userObj.userId;
+          onLoginSuccess();
+        }
+        else{
+          onLoginFailure();
+        }
+      });
+    }
+
+    function onLoginSuccess(){
+      $("#open_techmark").show();
+      $("#login_container").hide();
+      callClickAction();
+    }
+
+    function onLoginFailure(){
+      $("#open_techmark").hide();
+      $("#login_container").show();
+    }
+
+    function callClickAction(){
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs[0];
+        console.log("Sending msg");
+        chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+      });
+    }
+
+    function setStorage(obj){
+      chrome.storage.local.set({userObj : obj}, function() {
+        console.log('Value is set to ' + value);
+      });
+    }
+
+    function doLogin(){
+      var emailId = $("#emailId").val();
+      var password = $("#password").val();
+      $.ajax({
+        type : 'POST',
+        url : base_url+'user/login',
+        data : JSON.stringify({
+          username : emailId,
+          password : password
+        }),
+        dataType:"json",
+        contentType: "application/json",
+        success : function(result){
+          console.log(result);
+          loggedInUserId = result.userId;
+          setStorage(result);
+          onLoginSuccess();
+          showSuccessMsg("Welcome" + result.username);
+        },
+        error : function (error) {
+          console.log(error);
+          onLoginFailure();
+          showErrorMsg("Invalid Credentials");
+        }
+      });
+    }
+
     function postData(data){
         $.ajax({
             type : 'POST',
-            url : 'http://172.19.6.150:8080/post',
+            url : base_url+'post/'+loggedInUserId,
             data : JSON.stringify({
                 url : data.url,
                 imageUrl : data.image,
@@ -30,17 +101,25 @@
             contentType: "application/json",
             success : function(msg){
                 console.log(msg);
-                $("#success_msg").show();
+                showSuccessMsg("Article posted successfully.");
 
             },
             error : function (error) {
                 console.log(error);
-                $("#error_msg").show();
+                showErrorMsg("Oops something went wrong");
             }
         });
     }
 
-    function sendMessage(){
+    function showErrorMsg(msg){
+      $("#success_msg").hide();
+      $("#error_msg").show();
+      $("#error_msg").text(msg);
+    }
 
+    function showSuccessMsg(msg){
+      $("#success_msg").show();
+      $("#error_msg").hide();
+      $("#success_msg").text(msg);
     }
 })();
--- src/app/modules/user/components/home.component.ts
+++ src/app/modules/user/components/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { HomeService } from './home.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ConstantService } from '../../../core/services/constant.service';
 import { NavigationService } from '../../../core/services/navigation.service';
 
@@ -12,6 +13,8 @@ import { NavigationService } from '../../../core/services/navigation.service';
 })
 export class HomeComponent {
     constructor(
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
         private titleService : Title,
         private homeService: HomeService,
         private constantService: ConstantService,
@@ -26,6 +29,13 @@ export class HomeComponent {
     ngOnInit() {
         this.subscribeList();
     }
+
+  subscribeParams(){
+    this.activatedRoute.queryParams.subscribe(query=>{
+      console.log(query);
+      let userId = query.userId;
+    });
+  }
 
     subscribeList(category?) {
         this.pageCategory = category || 'ALL_SHARED';
